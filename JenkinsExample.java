f47ab3f2a1f121429a1316ae06770d8661211155
Darrin Visarraga <dVisarraga@users.noreply.github.com>
2020-03-18 16:00:52 -0600

    Update JenkinsExample.java
    Test #1 change

--- JenkinsExample.java
+++ JenkinsExample.java
@@ -4,9 +4,7 @@ public class JenkinsExample {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello World!");
-		System.out.println("Hello Darrin1!");
-		System.out.println("Hello Darrin2!");
-		System.out.println("Hello Darrin3!");
+		System.out.println("Hello Darrin Test#1!");
 	}
 
 }
