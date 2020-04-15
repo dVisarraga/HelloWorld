e12443e2a54755efacf1e24718a720daa3f68585
Darrin Visarraga <dVisarraga@users.noreply.github.com>
2020-04-15 17:34:39 -0600

    new commit

--- JenkinsExample.java
+++ JenkinsExample.java
@@ -4,9 +4,7 @@ public class JenkinsExample {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello World!");
-		System.out.println("Hello Darrin Test#1!");
-		System.out.println("Hello Darrin Test#2!");
-		System.out.println("Hello Darrin Test#3!");
+		System.out.println("Test#1!");
 	}
 
 }
